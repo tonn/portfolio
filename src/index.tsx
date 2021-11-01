eaecb6af6d46559375d0fd58c7dff91da4481f03
Anton Novikov <tonn.post@gmail.com>
2021-11-01 23:38:31 +0400

    fix: warnings

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { initGoogleAnalytics } from './analytics';
 
 export const DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
