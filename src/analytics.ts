fb73d64a8d406c5d73ed8fd7138250b2cf271382
Anton Novikov <tonn.post@gmail.com>
2021-11-01 23:32:56 +0400

    feat: added google analytics

---
+++ src/analytics.ts
@@ -0,0 +1,28 @@
+/*
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-82ENQHE562"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-82ENQHE562');
+  </script>
+*/
+
+export async function initGoogleAnalytics() {
+  const gtagScript = document.createElement('script');
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-82ENQHE562';
+  gtagScript.async = true;
+  document.head.appendChild(gtagScript);
+};
+
+const dataLayer: unknown[][] = (window as any).dataLayer || [];
+(window as any).dataLayer = dataLayer;
+
+export function gtag(...args: unknown[]) {
+  dataLayer.push(args);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-82ENQHE562');
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initGoogleAnalytics } from './analytics';
 
 export const DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+if (!DEV) {
+  initGoogleAnalytics();
+}
 
 ReactDOM.render(
   <App />,
@@ -14,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
